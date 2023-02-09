ce028d7663cbccfb285597df44700994942a3c74
Max Rusakovič <maxrusakovic@gmail.com>
2023-02-09 12:46:23 +0100

    [1] Initial release NASA Node.js project

---
+++ server/src/server.js
@@ -0,0 +1,24 @@
+const http = require("http");
+
+require("dotenv").config();
+
+const app = require("./app");
+const { mongoConnect } = require("./services/mongo");
+const { loadPlanetData } = require("./models/planets.model");
+const { loadLaunchData } = require("./models/lauches.model");
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+async function startServer() {
+  await mongoConnect();
+  await loadPlanetData();
+  await loadLaunchData();
+
+  server.listen(PORT, () => {
+    console.log(`Listening port ${PORT}...`);
+  });
+}
+
+startServer();
